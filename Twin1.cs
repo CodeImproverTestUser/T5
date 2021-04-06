67f68aa1dff1bbd1c1e83e919d496163f50d37b3
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-04-06 13:45:57 +0200

    Create Twin1.sc with 8 parameters

---
+++ Twin1.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace T5
+{
+    public class Twin1
+    {
+        int l1, l2, l3, l4, l5, l6, l7;
+        public Twin1(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int V8)
+        {
+            l1 = v1;
+            l2 = v2;
+            l3 = v3;
+            l4 = v4;
+            l5 = v5;
+            l6 = v6;
+            l7 = v7;
+            l8 = v8; 
+        }
+    }
+}
