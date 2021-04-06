479d3bca27c279f783ef6591ae579f998b866638
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-04-06 13:52:57 +0200

    Update Twin1.cs

--- Twin1.cs
+++ Twin1.cs
@@ -5,7 +5,7 @@ namespace T5
     public class Twin1
     {
         int l1, l2, l3, l4, l5, l6, l7;
-        public Twin1(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int V8)
+        public Twin1(int v1, int v2, int v3, int v4, int v5, int v6, int v7)
         {
             l1 = v1;
             l2 = v2;
@@ -13,8 +13,6 @@ namespace T5
             l4 = v4;
             l5 = v5;
             l6 = v6;
-            l7 = v7;
-            l8 = v8; 
         }
     }
 }
