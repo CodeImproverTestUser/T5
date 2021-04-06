75835eb7ca7a7b6215fd86c6124c401879858b5f
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-04-06 13:50:26 +0200

    Update CalculateClass.cs

--- CalculateClass.cs
+++ CalculateClass.cs
@@ -5,15 +5,9 @@ namespace T5
     public class CalculateClass
     {
         int l1, l2, l3, l4, l5, l6, l7;
-        public CalculateClass(int v1, int v2, int v3, int v4, int v5, int v6, int v7)
+        public CalculateClass(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int V8)
         {
-            l1 = v1;
-            l2 = v2;
-            l3 = v3;
-            l4 = v4;
-            l5 = v5;
-            l6 = v6;
-            l7 = v7;                     
+            l1 = v1;                               
         }
     }
 }
